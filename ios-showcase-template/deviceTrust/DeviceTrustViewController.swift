a8d7004732bd111d47f07e90d9913b7a11969180
Wei Li <wei.li@redhat.com>
2018-06-11 17:49:36 +0100

    💄 fix device trust view

--- ios-showcase-template/deviceTrust/DeviceTrustViewController.swift
+++ ios-showcase-template/deviceTrust/DeviceTrustViewController.swift
@@ -7,33 +7,37 @@
 
 import AGSSecurity
 import Foundation
+import Floaty
 
 protocol DeviceTrustListener {
     func performTrustChecks() -> [SecurityCheckResult]
 }
 
-/* The class for the table view cells. */
-class DeviceTrustTableViewCell: UITableViewCell {
-    @IBOutlet var name: UILabel!
-}
-
 /* The view controller for the device trust view. */
-class DeviceTrustViewController: UITableViewController {
+class DeviceTrustViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FloatyDelegate {
+    
+    let RED_COLOR = UIColor(named: "Red")
+    let GREEN_COLOR = UIColor(named: "Green")
+    
     var deviceTrustListener: DeviceTrustListener?
     var deviceChecks = [SecurityCheckResult]()
+    
     @IBOutlet var deviceTrustScore: UILabel!
-    let RED_COLOR = UIColor(red: CGFloat(150.0 / 255.0), green: CGFloat(44.0 / 255.0), blue: CGFloat(44.0 / 255.0), alpha: CGFloat(1.0))
-    let GREEN_COLOR = UIColor(red: CGFloat(57.0 / 255.0), green: CGFloat(180.0 / 255.0), blue: CGFloat(171.0 / 255.0), alpha: CGFloat(1.0))
+    @IBOutlet weak var deviceTrustScoreLabel: UILabel!
+    @IBOutlet weak var deviceTrustTestsNumberLabel: UILabel!
+    @IBOutlet weak var deviceTrustRerunButton: UIButton!
+    
+    @IBOutlet weak var deviceTrustTableView: UITableView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.renderRefreshButton()
+        
         // don't show empty table items
-        self.tableView.tableFooterView = UIView()
-
-        // cell row height
-        self.tableView.rowHeight = 75.0
-
+        self.deviceTrustTableView.tableFooterView = UIView()
+        
         // perform the detection checks
         self.performTrustChecks()
     }
@@ -44,67 +48,85 @@ class DeviceTrustViewController: UITableViewController {
     func performTrustChecks() {
         if let listener = self.deviceTrustListener {
             self.deviceChecks = listener.performTrustChecks()
-            self.tableView.reloadData()
+            self.deviceTrustTableView.reloadData()
             self.setTrustScore()
         }
+    }
+    
+    /**
+     - Render the refresh button in the view with the create/delete all actions
+     */
+    func renderRefreshButton() {
+        let floaty = Floaty()
+        floaty.sticky = true
+        floaty.buttonColor = UIColor(named: "Orange")!
+        floaty.buttonImage = UIImage(named: "ic_refresh_white")!
+        floaty.fabDelegate = self
+        self.view.addSubview(floaty)
     }
 
     /**
      - Set the trust score header value in the UI.
      */
     func setTrustScore() {
-        let totalTestFailures = self.deviceChecks.filter { !$0.passed }.count
-        let deviceTrustScore = 100 - ((Double(totalTestFailures) / Double(self.deviceChecks.count)) * 100)
-        self.deviceTrustScore?.text = "Device Trust Score: \(deviceTrustScore)%"
-
-        if deviceTrustScore == 100 {
-            self.deviceTrustScore.backgroundColor = GREEN_COLOR
-        } else {
-            self.deviceTrustScore.backgroundColor = RED_COLOR
-        }
+        self.deviceTrustTestsNumberLabel?.text = "(\(self.deviceChecks.count) Checks)"
+        let totalTestPassed = self.deviceChecks.filter { $0.passed }.count
+        let deviceTrustScore = Int(Double(totalTestPassed) / Double(deviceChecks.count) * 100)
+        self.deviceTrustScoreLabel?.text = "\(deviceTrustScore)%"
     }
-
+    
     /**
      - Set the number of sections required in the table
-
+     
      - Returns: The number of sections
      */
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     /**
      - Set the number of rows required in the section
-
+     
      - Returns: The number of device checks
      */
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.deviceChecks.count
     }
-
+    
     /**
      - Setup of the table view to reference the table in the storyboard
-
+     
      - Returns: An individual cell in the table list
      */
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "DeviceTrustTableViewCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! DeviceTrustTableViewCell
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "deviceTrustChecks"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
         // Fetches the appropriate note for the data source layout.
         let detection = self.deviceChecks[indexPath.row]
-
+        let imageView = cell.imageView!
+        let textView = cell.textLabel!
+        textView.isEnabled = true
+        
         // set the text colouring
         if detection.passed {
-            cell.textLabel?.text = detection.result
-            cell.textLabel?.textColor = GREEN_COLOR
-            cell.imageView?.image = UIImage(named: "ic_detected_false")
+            textView.text = detection.result
+            textView.textColor = GREEN_COLOR
+            imageView.image = UIImage(named: "ic_verified_user_white")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+            imageView.tintColor = GREEN_COLOR
         } else {
-            cell.textLabel?.text = detection.result
-            cell.textLabel?.textColor = RED_COLOR
-            cell.imageView?.image = UIImage(named: "ic_detected_true")
+            textView.text = detection.result
+            textView.textColor = RED_COLOR
+            imageView.image = UIImage(named: "ic_warning_white")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+            imageView.tintColor = RED_COLOR
         }
-
+        
         return cell
     }
+    
+    func emptyFloatySelected(_ floaty: Floaty) {
+        self.performTrustChecks()
+    }
+
+    
 }
